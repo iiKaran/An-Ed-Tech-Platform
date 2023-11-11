b79a858c343e98f902932b25bda809616537c75d
Karan Sehgal <karansehgal_01@Karans-MacBook-Air.local>
2023-11-11 18:07:20 +0530

    react-redux-setup completed for statemanagement

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import {Routes, Route} from "react-router-dom"
 import Home from "./Pages/Home";
 import Navbar from "./components/core/common/Navbar"
+import Signup from "./Pages/SignUp";
+import Login from "./Pages/Login";
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
    <Navbar/>
     <Routes>
-      <Route path="/" element={<Home/>}></Route>
+      <Route path="/" element={<Home/>}></Route> 
+      <Route path="/signup" element={<Signup/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
     </Routes>
     </div>
 
--- src/components/core/common/Navbar.jsx
+++ src/components/core/common/Navbar.jsx
@@ -4,20 +4,26 @@ import { Link, matchPath } from 'react-router-dom'
 import Logo from "./Logo-Full-Light.png"
 import { NavbarLinks } from './nav-data/navbar-links'
 import { useLocation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import {AiOutlineShoppingCart} from 'react-icons/ai'
 export default function Navbar() {
   const location = useLocation();
+  const {token} = useSelector((state)=>state.auth); 
+  const {cart} = useSelector((state)=>state.cart); 
+  const {user} = useSelector((state)=>state.profile);
+  const {totalItems} = useSelector((state)=> state.cart);
   const matchRoute= (route)=>{
     return matchPath({path:route},location.pathname)
   }
   return (
-    <div className='flex h-14 border-b-[1px]  border-b-richblack-700'>
+    <div className='flex mt-3 h-14 border-b-[1px]  border-b-richblack-700'>
 
-       <div className='w-[70vw] flex mx-auto border maz-w-max-content items-center justify-between'>
+       <div className='w-[90vh] lg:w-[70vw] flex mx-auto  maz-w-max-content items-center justify-between'>
       <Link to="/">
         <img src={Logo} width={160} height={42} loading='lazy' alt="" />
       </Link>
    
-      <nav>
+      <nav className='hidden lg:block'>
         <ul className='flex gap-x-6 text-richblack-25'>
          {
          NavbarLinks.map((element , index)=>(
@@ -37,10 +43,40 @@ export default function Navbar() {
         </ul>
       </nav>
   
-     <div className="btns flex gap-x-4 items-center">
+     <div className="btns flex gap-x-4  items-center">
       {/* login/signup/dashboard */}
-
-
+      {
+                user && user?.accountType != "Instructor" && (
+                    <Link to="/dashboard/cart" className='relative'>
+                        <AiOutlineShoppingCart />
+                        {
+                            totalItems > 0 && (
+                                <span>
+                                    {totalItems}
+                                </span>
+                            )
+                        }
+                    </Link>
+                )
+            }
+      {
+                token === null && (
+                    <Link to="/login">
+                        <button className='border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 rounded-md'>
+                            Log in
+                        </button>
+                    </Link>
+                )
+      }
+            {
+                token === null && (
+                    <Link to="/signup">
+                        <button  className='border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 rounded-md'>
+                            Sign Up
+                        </button>
+                    </Link>
+                )
+            }
      </div>
 
       </div>        
